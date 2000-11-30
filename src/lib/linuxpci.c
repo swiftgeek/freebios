11d3c0db414c13660fccef2c22a22dab2caae25e
rminnich <rminnich>
2000-11-30 22:06:42 +0000

    Added support for 64-bit base address registers.

--- src/lib/linuxpci.c
+++ src/lib/linuxpci.c
@@ -1,5 +1,5 @@
 /*
- *    $Id: linuxpci.c,v 1.4 2000/10/19 23:12:13 rminnich Exp $
+ *    $Id: linuxpci.c,v 1.5 2000/11/30 22:06:43 rminnich Exp $
  *
  *      PCI Bus Services, see include/linux/pci.h for further explanation.
  *
@@ -179,6 +179,7 @@ void pci_read_bases(struct pci_dev *dev, unsigned int howmany)
 
 		addr &= (PCI_BASE_ADDRESS_SPACE | PCI_BASE_ADDRESS_MEM_TYPE_MASK);
 		if (addr == (PCI_BASE_ADDRESS_SPACE_MEMORY | PCI_BASE_ADDRESS_MEM_TYPE_64)) {
+			DBG("reg %d is 64-bit\n", reg);
 			/* this is a 64-bit memory base address */
 			reg++;
 			pci_read_config_dword(dev, PCI_BASE_ADDRESS_0 + (reg << 2), &addr);
