6e9aecd0c058cee0aa34b6fe045ba14fa3caa2c4
ollie <ollie>
2001-05-21 05:39:35 +0000

    fix some typo

--- src/northsouthbridge/sis/550/southbridge.c
+++ src/northsouthbridge/sis/550/southbridge.c
@@ -1,13 +1,13 @@
 
 /*
  * Bootstrap code for the INTEL 
- * $Id: southbridge.c,v 1.3 2001/05/21 04:12:04 ollie Exp $
+ * $Id: southbridge.c,v 1.4 2001/05/21 05:39:35 ollie Exp $
  *
  */
 
 #ifndef lint
 static char rcsid[] =
-"$Id: southbridge.c,v 1.3 2001/05/21 04:12:04 ollie Exp $";
+"$Id: southbridge.c,v 1.4 2001/05/21 05:39:35 ollie Exp $";
 #endif
 
 
@@ -103,8 +103,8 @@ south_fixup(void)
 	pcidev = pci_find_device(PCI_VENDOR_ID_SI, PCI_DEVICE_ID_SI_503, (void *)NULL);
 	if (pcidev != NULL) {
 	    u8 reg;
-	    pci_read_config_word(pcidev, 0x77, &temp);
-	    pci_write_config_byte(pci_dev, 0x77, temp & 0xEF);
+	    pci_read_config_byte(pcidev, 0x77, &reg);
+	    pci_write_config_byte(pcidev, 0x77, reg & 0xEF);
 	}
 }
 
