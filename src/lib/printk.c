70d9f7b80b2718dc59a8aae9f3e1d54515a8788b
rminnich <rminnich>
2001-08-29 16:22:46 +0000

    Make log_buf global so I know where it is and can watch it with the arium.
    Fix m754lmr/Config so it works with DoC

--- src/lib/printk.c
+++ src/lib/printk.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char rcsid[] = "$Id: printk.c,v 1.3 2001/08/08 02:45:09 ebiederm Exp $";
+static char rcsid[] = "$Id: printk.c,v 1.4 2001/08/29 16:22:46 rminnich Exp $";
 #endif
 
 //typedef void * va_list;
@@ -14,7 +14,7 @@ static char rcsid[] = "$Id: printk.c,v 1.3 2001/08/08 02:45:09 ebiederm Exp $";
 #include <stdarg.h>
 #include <smp/spinlock.h>
 
-static char buf[1024];
+char log_buf[1024];
 
 /* printk's without a loglevel use this.. */
 #define DEFAULT_MESSAGE_LOGLEVEL 4 /* BIOS_WARNING */
@@ -50,10 +50,10 @@ int do_printk(int msg_level, const char *fmt, ...)
 	spin_lock(&console_lock);
 
 	va_start(args, fmt);
-	i = vsprintf(buf, fmt, args); /* hopefully i < sizeof(buf)-4 */
+	i = vsprintf(log_buf, fmt, args); /* hopefully i < sizeof(log_buf)-4 */
 	va_end(args);
 
-	display(buf);
+	display(log_buf);
 
 	spin_unlock(&console_lock);
 
