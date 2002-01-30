098648a5ae5cf0956388fa06e223e37681d8239e
rminnich <rminnich>
2002-01-30 20:50:53 +0000

    Fixes for potential divide-by-zero in setup_com in some superios. Not really needed, but in the BIOS paranoia is a healthy frame of mind.
    Added some debug prints to newpci.c
    
    Fixed sbc710 Config to ensure that com1 and com2 are enabled
       (input still does not work)
    
    Changed
    extern struct ... *all_superio in hardwaremain.c to
    extern struct ... all_superio[] to avoid gcc dereferencing the pointer
       (when did THIS C behavior change go in?)

--- src/arch/i386/lib/hardwaremain.c
+++ src/arch/i386/lib/hardwaremain.c
@@ -23,13 +23,13 @@ it with the version available from LANL.
 
 /*
  * C Bootstrap code for the INTEL 
- * $Id: hardwaremain.c,v 1.14 2002/01/29 20:28:23 ebiederm Exp $
+ * $Id: hardwaremain.c,v 1.15 2002/01/30 20:50:53 rminnich Exp $
  *
  */
 
 #define LINUXBIOS
 #ifndef lint
-static char rcsid[] = "$Id: hardwaremain.c,v 1.14 2002/01/29 20:28:23 ebiederm Exp $";
+static char rcsid[] = "$Id: hardwaremain.c,v 1.15 2002/01/30 20:50:53 rminnich Exp $";
 #endif
 
 #include <arch/io.h>
@@ -205,7 +205,7 @@ void hardwaremain(int boot_complete)
 	 * for now -- rgm. 
 	 */
 #ifdef USE_NEW_SUPERIO_INTERFACE
-	extern struct superio *all_superio;
+	extern struct superio all_superio[];
 	extern int nsuperio;
 	extern void handle_superio(int pass, struct superio *s, int nsuperio);
 #endif
